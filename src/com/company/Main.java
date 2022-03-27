a820611b74aed62c84854b84745ec7fd1011c693
dezzaired <igorkawa99@gmail.com>
2022-03-27 18:16:36 +0200

    Interface has one variable Class implements interface and return sum(salary,number) In main class we create an instance of our interface and then we display method getSalary. The tab was created to store a few numbers, then we sort them and display with for each.

---
+++ src/com/company/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Interfejsik a = new Klasunia();
+        System.out.println(a.getSalary());
+
+        int[] tab = {4,7,-1,3,15};
+        Arrays.sort(tab);
+        for(int val : tab) {
+            System.out.println(val);
+        }
+    }
+}
+interface Interfejsik
+{
+    double number = 3.54;
+    double getSalary();
+}
+class Klasunia implements Interfejsik
+{
+    double salary = 5200;
+
+    @Override
+    public double getSalary() {
+        return salary+number;
+    }
+}
